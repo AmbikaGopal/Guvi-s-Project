3c539a7590aee9eb740a60bb4ac223711b4fd259
AmbikaGopal <ambika2773@gmail.com>
2025-06-29 21:21:02 +0530

    Delete strm_ploting.py

--- strm_ploting.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-st.title('Plotting using streamlit')
-data=pd.DataFrame(np.random.randn(100,3), columns=['A','B','C'])
-# st.write(data)
-# st.table(data)
-st.line_chart(data)
